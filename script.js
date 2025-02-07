f5b62b28675a6939a78c5245d92be0dfac96479a
pietereb <pieterthecoder@proton.me>
2025-02-07 08:03:01 +0100

    Fix grid pattern and added forEach loop
    Reworked how the grid is produced: now as a row for loop within
    a column for loop. Elements within the grid are called with
    querySelectorAll and looped over with the forEach function.
    Etch-a-sketch now works with a single color and size.

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 let container = document.querySelector(".container")
 
+// Create a grid of NxN
 for (i = 0; i < 16; i++){
-    let square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
+    let column = document.createElement("div");
+    column.classList.add("column");
+    container.appendChild(column);
     for (j = 0; j < 16; j++){
         let row = document.createElement("div");
-        row.classList.add("square");
-        square.appendChild(row);
+        row.classList.add("row");
+        column.appendChild(row);
     };
 };
 
+// Event triggering on mouse over
+let allSquares = document.querySelectorAll(".row");
+
+allSquares.forEach((square) => {
+    square.addEventListener('mouseenter', () => {
+      square.style.backgroundColor = "purple";
+    });
+  });
+
+// for (k = 0; k < allSquares.length; k++){
+//     allSquares[k].addEventListener('mouseenter', () => {
+//         console.log(`hovered square ${k}`);
+//         allSquares[4].style.backgroundColor = "purple";
+//     });
+// }
+
+
+
+// let fourthSquare = allSquares[3];
+// console.log(fourthSquare);
+// fourthSquare.style.backgroundColor = "blue";
+// allSquares[5].style.backgroundColor = "purple";
