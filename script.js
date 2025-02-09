2fcbf38380a3b2cb98ab0164f62defece82bd479
pietereb <pieterthecoder@proton.me>
2025-02-09 09:41:19 +0100

    Fix reset button and add other functionalities
    The bug with reset button was fixed such that game can now be
    properly restarted. Added buttons for functionalities like:
    change random colour, use random colours throughout and progressive
    colouring.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let container = document.querySelector(".container")
+let container = document.querySelector(".container");
 
 // Create a grid of NxN
 for (i = 0; i < 16; i++){
@@ -8,29 +8,64 @@ for (i = 0; i < 16; i++){
     for (j = 0; j < 16; j++){
         let row = document.createElement("div");
         row.classList.add("row");
+        row.style.backgroundColor = "white";
         column.appendChild(row);
     };
 };
 
-// Event triggering on mouse over
-let allSquares = document.querySelectorAll(".row");
+// Create reset button
+reset = document.querySelector(".reset");
+reset.addEventListener('click', () =>{
+    console.log("clicked");
+    let userChoice = prompt("How many squares?");
+    console.log(userChoice);
+    if (userChoice > 0 && userChoice < 101){
 
+        let allColumns = document.querySelectorAll(".column");
+        allColumns.forEach((column) => {
+            column.remove();
+          });
+
+        for (i = 0; i < userChoice; i++){
+            let column = document.createElement("div");
+            column.classList.add("column");
+            container.appendChild(column);
+            for (j = 0; j < userChoice; j++){
+                let row = document.createElement("div");
+                row.classList.add("row");
+                row.style.backgroundColor = "white";
+                column.appendChild(row);
+            };
+        };
+
+        let allSquares = document.querySelectorAll(".row");
+        allSquares.forEach((square) => {
+            square.addEventListener('mouseenter', () => {
+              square.style.backgroundColor = `hsl(${RANDOMCOLOUR}, 100%, 50%)`;
+            });
+          });
+
+    } else {
+        alert("Invalid Choice! Pick a number between 1 and 100.");
+    };
+}
+);
+
+// Event triggering on mouse over using a fixed colour
+const RANDOMCOLOUR = Math.random()*360;
+let allSquares = document.querySelectorAll(".row");
 allSquares.forEach((square) => {
     square.addEventListener('mouseenter', () => {
-      square.style.backgroundColor = "purple";
+      square.style.backgroundColor = `hsl(${RANDOMCOLOUR}, 100%, 50%)`;
     });
   });
 
-// for (k = 0; k < allSquares.length; k++){
-//     allSquares[k].addEventListener('mouseenter', () => {
-//         console.log(`hovered square ${k}`);
-//         allSquares[4].style.backgroundColor = "purple";
+// Event triggering on mouse over using a random colour
+// let allSquares = document.querySelectorAll(".row");
+// allSquares.forEach((square) => {
+//     square.addEventListener('mouseenter', () => {
+//       square.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`;
 //     });
-// }
+//   });
 
 
-
-// let fourthSquare = allSquares[3];
-// console.log(fourthSquare);
-// fourthSquare.style.backgroundColor = "blue";
-// allSquares[5].style.backgroundColor = "purple";
